35cc469c34e6fdacf2376759797837c0f5153d44
Mary Cutrali <mary.cutrali@livingsocial.com>
2012-04-03 17:46:19 -0400

    error messages in user-create

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   authenticates_with_sorcery!
   attr_accessible :display_name, :email_address, :full_name, :password, :password_confirmation
-  validates :full_name, :presence => true
+  validates :full_name, :presence => { :message => "Full name can not be blank.  Please enter your full name." }
   validates :display_name, :allow_nil => true, :length => { :minimum => 2, :maximum => 32 }
   validates :email_address, :presence => true, :format => {:with => /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/}, :uniqueness => true
   validates_length_of :password, :minimum => 3, :message => "password must be at least 3 characters long", :if => :password
