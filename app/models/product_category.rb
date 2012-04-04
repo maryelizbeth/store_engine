c0335482b04cd3696224940e0be52f038375aaf0
Mary Cutrali <mary.cutrali@livingsocial.com>
2012-04-04 14:27:42 -0400

    make product category names unique

--- app/models/product_category.rb
+++ app/models/product_category.rb
@@ -2,4 +2,5 @@ class ProductCategory < ActiveRecord::Base
   attr_accessible :name
   has_many :categorizations 
   has_many :products, :through => :categorizations
+  validate_uniqueness_of :name
 end
