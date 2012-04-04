89595adfcc0818188744b203cb2f561956875ad3
Conan Rimmer <conanrimmer@gmail.com>
2012-04-03 21:39:53 -0400

    auto login on user account create

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -46,6 +46,7 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
+        auto_login(@user)
         format.html { redirect_to(:users, notice: 'User was successfully created.') }
         # format.json { render json: @user, status: :created, location: @user }
       else
