3278df42a244a390b101e6060dda847d94977014
Mary Cutrali <mary.cutrali@livingsocial.com>
2012-04-04 16:06:37 -0400

    add product#thumbnail with default image

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,4 +6,10 @@ class Product < ActiveRecord::Base
   validates_uniqueness_of :title 
   validates :price, :numericality => { :greater_than => 0 }, :presence => true
   validates :photo_url, :allow_nil => true, :format => { :with => /^https?:\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?(?:png|jpe?g|gif|svg)$/ }
+
+  DEFAULT_IMAGE_PATH = "/no_image.jpg"
+
+  def thumbnail
+    photo_url || DEFAULT_IMAGE_PATH
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,10 @@
 StoreEngine::Application.routes.draw do
-  
-  get "store/index"
-
-  resources :product_categories
-
-root :to => 'users#index'
+get "store/index"
+root :to => 'store#index'
 resources :user_sessions
 resources :users
 resources :products
+resources :product_categories
 
 match 'login' => 'user_sessions#new', :as => :login
 match 'logout' => 'user_sessions#destroy', :as => :logout
