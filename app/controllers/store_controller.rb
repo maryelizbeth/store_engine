1ee758a3b1e1734a24b38394d85e8ce5f41dcdf5
Mary Cutrali <mary.cutrali@livingsocial.com>
2012-04-05 13:37:05 -0400

    tie cart to session and add ability to move products into cart

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,7 @@
 class StoreController < ApplicationController
-  # skip_before_filter :require_login, :only => [:index]
+  skip_before_filter :require_login
+  before_filter :find_cart_from_session
+
   def index
     if params[:category_id] && !params[:category_id].empty?
       @products = ProductCategory.find(params[:category_id]).products
@@ -10,17 +12,15 @@ class StoreController < ApplicationController
   end
 
   def add_to_cart
-    if current_user
-      if !current_user.has_a_shopping_cart?
-        Order.create!(:user_id => current_user.id)
-      end
-      product = Product.find(params[:product_id])
-      OrderProduct.create!(
-        :order_id => current_user.shopping_cart.id,
-        :product_id => product.id,
-        :price => product.price,
-        :quantity => params[:quantity])
-    end
+    @cart.add_product_to_cart(params[:product_id])
+    session[:cart_id] = @cart.id
     redirect_to store_index_path
+  end
+
+  private
+
+  def find_cart_from_session
+    @cart = Cart.find_by_id(session[:cart_id]) if session[:cart_id]
+    @cart ||= Cart.create
   end
 end
