294b1780fa9a1836224958f9082b346348e78fec
Conan Rimmer <conanrimmer@gmail.com>
2012-04-10 14:53:35 -0400

    fix photo_url issue where it could not be blank

--- app/models/product.rb
+++ app/models/product.rb
@@ -9,7 +9,7 @@ class Product < ActiveRecord::Base
   validates_presence_of :title, :description
   validates_uniqueness_of :title 
   validates :price, :numericality => { :greater_than => 0 }, :presence => true
-  validates :photo_url, :allow_nil => true, :format => { :with => /^(https?:\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?(?:png|jpe?g|gif|svg))$/ }
+  validates :photo_url, :allow_blank => true, :format => { :with => /^(https?:\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?(?:png|jpe?g|gif|svg))$/ }
 
   DEFAULT_IMAGE_PATH = "/no_image.jpg"
 
