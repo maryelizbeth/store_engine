8935db2242eb65fd2f22718f6467b5ade5fce6b3
Conan Rimmer <conanrimmer@gmail.com>
2012-04-10 15:51:39 -0400

    change product fabricator to captilize product names and use random decimals for price

--- spec/fabricators/product_fabricator.rb
+++ spec/fabricators/product_fabricator.rb
@@ -1,5 +1,5 @@
 Fabricator(:product, :class_name => "Product") do 
-   title        { Faker::Lorem.words.join(" ") }
+   title        { Faker::Lorem.words.each(&:capitalize!).join(" ") }
    description  { Faker::Lorem.paragraphs(1) }
-   price 1.75
+   price        { (rand(99999)+1)/100.0 }
 end 
