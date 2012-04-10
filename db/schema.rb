d7717f27dc66cb1b75c1062e0b51cbebc1e45394
Conan Rimmer <conanrimmer@gmail.com>
2012-04-09 21:06:12 -0400

    remove duplicate migration for is_admin and unused specs

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120406113915) do
+ActiveRecord::Schema.define(:version => 20120405210633) do
 
   create_table "cart_products", :force => true do |t|
     t.integer  "cart_id"
