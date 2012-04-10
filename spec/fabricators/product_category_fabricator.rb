84c5ccec64fd0a70da72db3cda3ad5e7a1b8989e
Conan Rimmer <conanrimmer@gmail.com>
2012-04-10 15:52:33 -0400

    change product category fabricator to cap names and use only 2 in order to distinguish between cat names and prod names

--- spec/fabricators/product_category_fabricator.rb
+++ spec/fabricators/product_category_fabricator.rb
@@ -1,3 +1,3 @@
 Fabricator(:product_category, :class_name => "ProductCategory") do 
-   name        { Faker::Lorem.words.join(" ") }
+   name        { Faker::Lorem.words(2).each(&:capitalize!).join(" ") }
 end
