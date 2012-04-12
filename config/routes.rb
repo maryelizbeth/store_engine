92a0eec4b979331dc95badd54a549d5f0b878a17
Mary Cutrali <mary.cutrali@livingsocial.com>
2012-04-12 11:14:47 -0400

    added contact information page

--- config/routes.rb
+++ config/routes.rb
@@ -21,7 +21,7 @@ post  "cart/convert_cart_to_order", :as => :convert_cart_to_order
 post  "cart/add_to_cart", :as => :add_to_cart
 
 get   "store/index"
-
+get   "store/contact_us"
 match 'login' => 'user_sessions#new', :as => :login
 match 'logout' => 'user_sessions#destroy', :as => :logout
 end
