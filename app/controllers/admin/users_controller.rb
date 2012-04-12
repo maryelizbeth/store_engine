cec14ccc05a5bddba2091096b99a10df2df19fab
Conan Rimmer <conanrimmer@gmail.com>
2012-04-12 10:40:27 -0400

    allow admins to view all users and single user

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,4 +1,11 @@
 class Admin::UsersController < ApplicationController
   before_filter :require_admin
-
+  
+  def index
+    @users = User.all
+  end
+  
+  def show
+    @user = User.find(params[:id])
+  end
 end
