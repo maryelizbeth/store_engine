0c5992c02c54b309d3bfb7b971f05abe0e2207ab
Conan Rimmer <conanrimmer@gmail.com>
2012-04-12 15:00:36 -0400

    simple styling for order products in order#show

--- app/models/order_product.rb
+++ app/models/order_product.rb
@@ -2,4 +2,8 @@ class OrderProduct < ActiveRecord::Base
   attr_accessible :order_id, :price, :product_id, :quantity
   belongs_to :order 
   belongs_to :product
+  
+  def total
+    quantity * price
+  end
 end
