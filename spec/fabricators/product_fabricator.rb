66573fce54dbde6367db993c6a71a236e81c949b
Conan Rimmer <conanrimmer@gmail.com>
2012-04-15 15:19:10 -0400

    added tests to cover product#show

--- spec/fabricators/product_fabricator.rb
+++ spec/fabricators/product_fabricator.rb
@@ -1,5 +1,5 @@
 Fabricator(:product, :class_name => "Product") do 
    title        { Faker::Lorem.words.each(&:capitalize!).join(" ") }
-   description  { Faker::Lorem.paragraphs(1) }
+   description  { Faker::Lorem.paragraphs(1).first }
    price        { (rand(99999)+1)/100.0 }
 end 
