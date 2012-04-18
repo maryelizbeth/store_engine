b534e743667eaeb2ea593c102c5974ed248e06a3
Conan Rimmer <conanrimmer@gmail.com>
2012-04-18 08:06:59 -0400

    added order products to the order flyout

--- spec/requests/admin_orders_details_request_spec.rb
+++ spec/requests/admin_orders_details_request_spec.rb
@@ -16,9 +16,11 @@ describe "Admin Orders Details flyout" do
       let!(:order_1)    { Fabricate(:order, :user_id => user_1.id) }
       let!(:order_product_1)  { Fabricate(:order_product, :order => order_1, :product => product_1, :price => product_1.price) }
       let!(:order_2)    { Fabricate(:order, :user_id => user_2.id) }
-      let!(:order_product_2)  { Fabricate(:order_product, :order => order_2, :product => product_2, :price => product_2.price) }
+      let!(:order_product_2)  { Fabricate(:order_product, :order => order_2, :product => product_1, :price => product_1.price, :quantity => 3) }
+      let!(:order_product_3)  { Fabricate(:order_product, :order => order_2, :product => product_2, :price => product_2.price) }
+      let!(:purchased_items)  { [order_product_2, order_product_3] }
       let!(:order_3)    { Fabricate(:order, :user_id => user_1.id) }
-      let!(:order_product_3)  { Fabricate(:order_product, :order => order_3, :product => product_2, :price => product_2.price) }
+      let!(:order_product_4)  { Fabricate(:order_product, :order => order_3, :product => product_2, :price => product_2.price) }
       let!(:orders)     { [order_1, order_2, order_3] }
 
       context "on the admin orders index page" do
@@ -41,6 +43,17 @@ describe "Admin Orders Details flyout" do
 
           it "displays the total for the order" do
             find("#order_#{order_2.id}_details_total").text.should have_content number_to_currency(order_2.total)
+          end
+
+          context "for the items purchased" do
+            it "displays the correct information" do
+              purchased_items.each do |pi|
+                find("#order_item_#{pi.id}_title").text.should have_content pi.product.title
+                find("#order_item_#{pi.id}_price").text.should have_content number_to_currency(pi.price)
+                find("#order_item_#{pi.id}_quantity").text.should have_content pi.quantity
+                find("#order_item_#{pi.id}_total").text.should have_content number_to_currency(pi.total)
+              end
+            end
           end
         end
       end
