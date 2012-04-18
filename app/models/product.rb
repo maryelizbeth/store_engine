4e9041b598016a88a07c7cb7cde2b9e64e789fc6
Conan Rimmer <conanrimmer@gmail.com>
2012-04-18 10:40:30 -0400

    better handling around no image state for product

--- app/models/product.rb
+++ app/models/product.rb
@@ -11,10 +11,14 @@ class Product < ActiveRecord::Base
   validates :price, :numericality => { :greater_than => 0 }, :presence => true
   validates :photo_url, :allow_blank => true, :format => { :with => /^(https?:\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?(?:png|jpe?g|gif|svg))$/ }
 
-  DEFAULT_IMAGE_PATH = "/no_image.jpg"
+  DEFAULT_IMAGE_PATH = "/assets/no_image.jpg"
 
   def thumbnail
-    photo_url || DEFAULT_IMAGE_PATH
+    unless photo_url.nil? || photo_url.blank?
+      photo_url
+    else
+      DEFAULT_IMAGE_PATH
+    end
   end
 
   def has_photo?
